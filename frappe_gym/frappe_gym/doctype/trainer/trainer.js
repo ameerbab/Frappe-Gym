05346bd12a60ea5e2f5536559bd4cef9d78c4b72
Finbyz <info@finbyz.tech>
2023-04-12 10:27:20 +0530

    first commit

--- frappe_gym/frappe_gym/doctype/trainer/trainer.js
+++ frappe_gym/frappe_gym/doctype/trainer/trainer.js
@@ -5,9 +5,7 @@ frappe.ui.form.on("Trainer", {
     full_name_concate:function(frm){
        frm.set_value("full_name", frm.doc.first_name +" "+frm.doc.last_name)
     },
-    first_name:function(frm){
-        frm.trigger('full_name_concate')
-    },
+   
     last_name:function(frm){
         frm.trigger('full_name_concate')
     }
