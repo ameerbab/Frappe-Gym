2606d0cb86588f6e00aef2b541e59a9977459e6a
Finbyz <info@finbyz.tech>
2023-04-27 17:59:52 +0530

    changes

--- frappe_gym/frappe_gym/page/member_profile_detai/member_profile_detai.js
+++
@@ -1,6 +0,0 @@
-frappe.pages["member-profile-page"].refresh= function (wrapper) {
-	frappe.require("member_profile_details_controller.js", () => {
-		let user_profile = new frappe.ui.UserProfile(wrapper);
-		user_profile.show();
-	});
-};
